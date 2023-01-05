5f74c29437e684cd9ecb2cd6577ec5330d96da47
Tomazacre12 <shadow12122004@gmail.com>
2023-01-05 21:01:33 +0700

    Them quasar app bai 1

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title class="absolute-center">
+          SmackChat
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { openURL } from 'quasar';
+
+export default defineComponent({
+  computed: {
+    title() {
+      console.log(this.$route)
+    }
+  },
+  methods: {
+    openURL
+  }
+});
+</script>
